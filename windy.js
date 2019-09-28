bc4f52792a68b52cdeb887781ead1683f1473369
OlgaGao <55299869+OlgaGao@users.noreply.github.com>
2019-09-28 13:58:26 -0700

    Create windy.js

---
+++ windy.js
@@ -0,0 +1,22 @@
+
+function draw() {
+  stroke(random(60, 70), 100, 90);
+  line(x-30, height-10, x+random(-10, 10), height-10-random(h));
+  noStroke();
+
+  x = x + 10;
+
+  if (x > width) {
+    x = random(10);
+    h = h + 10;
+  }
+
+  if (random(100) > 99.9) {
+    fill(255);
+    rect(0, 0, width, height-40);
+    h = 10;
+  }
+
+  fill(40, 100, 60);
+  rect(0, height-10, width, 10);
+}
